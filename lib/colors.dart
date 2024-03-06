c2b769e608edbd3ce9fa730be92d219075244c94
Ufuk Öztop <ufuk.oztop@pingpong.university>
2024-03-06 14:25:25 +0300

    init

---
+++ lib/colors.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class AppLightColors{
+  static get appWhite => Colors.white;
+  static get appWhite12 => Colors.white12;
+  static get appBlack => Colors.black;
+  static get appGrey => Colors.grey;
+  static get appLightGrey => Color(0xFFE7E7E7);
+  static get appRed => Colors.red;
+  static get appDarkBlue => const Color(0xFF051726);
+  static get appGreen =>  Colors.green;
+  static get appLightGreen => const Color(0xFF81FFD9);
+  static get appTransparent =>  Colors.transparent;
+}
